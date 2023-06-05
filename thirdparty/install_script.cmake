1fa2bf8bdf463a741de0bab15431ac9e64048683
cosine <sak9188@163.com>
2023-06-05 14:08:21 +0800

    继续修复CMake

--- thirdparty/install_script.cmake
+++ thirdparty/install_script.cmake
@@ -7,9 +7,8 @@ if(__WIN__)
     set(CMAKE_BUILD_TOOLS "Visual Studio 16 2019")
     set(CMAKE_BUILD_FLAGS "-A x64")
 elseif(__ANDROID__)
-    # TODO: 这里有可能是其他的构建工具，不过先不管了
     set(CMAKE_BUILD_TOOLS "Ninja")
-    set(CMAKE_BUILD_FLAGS "-DCFLAGS=-std=c++20 -m64")
+    set(CMAKE_BUILD_FLAGS "")
 elseif(__LINUX__)
     set(CMAKE_BUILD_TOOLS "Ninja")
     set(CMAKE_BUILD_FLAGS "-DCFLAGS=-std=c++20 -m64")
@@ -19,59 +18,94 @@ elseif(__MAC__)
 endif()
 
 macro(install_library lib_name lib_defines)
-    message(STATUS "build_library: ${lib_name}")
+
+message(STATUS "build_library: ${lib_name}")
+if(NOT DEFINED __ANDROID__)
+    set(build_command
+    "cmake . -G \"${CMAKE_BUILD_TOOLS}\" -B ./build_${CMAKE_SYSTEM_NAME}/ ${CMAKE_BUILD_FLAGS} ${CMAKE_NEXT_LINE}
+-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                                                           ${CMAKE_NEXT_LINE}
+-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}                                                   ${CMAKE_NEXT_LINE}
+-DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}                                                   ${CMAKE_NEXT_LINE}
+-DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}                                                   ${CMAKE_NEXT_LINE}
+-DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}                                           ${CMAKE_NEXT_LINE}
+-DBUILD_STATIC_LIBS=ON                                                                           ${CMAKE_NEXT_LINE}
+-DBUILD_SHARED_LIBS=ON                                                                           ${CMAKE_NEXT_LINE}
+-DENABLE_TESTING=OFF                                                                             ${CMAKE_NEXT_LINE}
+-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}                                                         ${CMAKE_NEXT_LINE}
+-DCMAKE_INCLUDE_PATH=${CMAKE_PREFIX_PATH}/include                                                ${CMAKE_NEXT_LINE}
+-DCMAKE_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib                                                    ${CMAKE_NEXT_LINE}
+-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}                                                           ${CMAKE_NEXT_LINE}
+-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}                                                       ${CMAKE_NEXT_LINE}
+${lib_defines}
+")
+else()
     set(build_command 
-        cmake . -G "${CMAKE_BUILD_TOOLS}" -B ./build/ ${CMAKE_BUILD_FLAGS}
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-        -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
-        -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}
-        -DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}
-        -DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}
-        -DBUILD_STATIC_LIBS=ON
-        -DBUILD_SHARED_LIBS=ON
-        -DENABLE_TESTING=OFF
-        -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}
-        -DCMAKE_INCLUDE_PATH=${CMAKE_PREFIX_PATH}/include
-        -DCMAKE_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib
-        -DCAMKE_C_COMPILER=${CMAKE_C_COMPILER}
-        -DCAMKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-        ${lib_defines}
-    )
+        "cmake . -G \"${CMAKE_BUILD_TOOLS}\" -B ./build_${CMAKE_SYSTEM_NAME}/ ${CMAKE_BUILD_FLAGS} ${CMAKE_NEXT_LINE}
+-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}                                                               ${CMAKE_NEXT_LINE}
+-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}                                                       ${CMAKE_NEXT_LINE}
+-DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}                                                       ${CMAKE_NEXT_LINE}
+-DCMAKE_INSTALL_BINDIR=${CMAKE_INSTALL_BINDIR}                                                       ${CMAKE_NEXT_LINE}
+-DCMAKE_INSTALL_INCLUDEDIR=${CMAKE_INSTALL_INCLUDEDIR}                                               ${CMAKE_NEXT_LINE}
+-DBUILD_STATIC_LIBS=ON                                                                               ${CMAKE_NEXT_LINE}
+-DBUILD_SHARED_LIBS=ON                                                                               ${CMAKE_NEXT_LINE}
+-DENABLE_TESTING=OFF                                                                                 ${CMAKE_NEXT_LINE}
+-DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}                                                             ${CMAKE_NEXT_LINE}
+-DCMAKE_INCLUDE_PATH=${CMAKE_PREFIX_PATH}/include                                                    ${CMAKE_NEXT_LINE}
+-DCMAKE_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib                                                        ${CMAKE_NEXT_LINE}
+-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}                                                               ${CMAKE_NEXT_LINE}
+-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}                                                           ${CMAKE_NEXT_LINE}
+-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}                                                       ${CMAKE_NEXT_LINE}
+-DCMAKE_MAKE_PROGRAM=${CMAKE_MAKE_PROGRAM}                                                           ${CMAKE_NEXT_LINE}
+-DANDROID_ABI=${ANDROID_ABI}                                                                         ${CMAKE_NEXT_LINE}
+-DANDROID_PLATFORM=${ANDROID_PLATFORM}                                                               ${CMAKE_NEXT_LINE}
+-DANDROID_NDK=${ANDROID_NDK}                                                                         ${CMAKE_NEXT_LINE}
+-DANDROID_TOOLCHAIN=${ANDROID_TOOLCHAIN}                                                             ${CMAKE_NEXT_LINE}
+-DANDROID_STL=${ANDROID_STL}                                                                         ${CMAKE_NEXT_LINE}
+-DANDROID_CPP_FEATURES=${ANDROID_CPP_FEATURES}                                                       ${CMAKE_NEXT_LINE}
+${lib_defines}
+")
+endif()
 
-    message(STATUS "build_command: ${build_command}")
-    execute_process(
-        COMMAND ${build_command}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}
-        RESULT_VARIABLE build_result
-        OUTPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}/build.log
-    )
-    message(STATUS "work directory: ${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}")
+write_file(${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}/build.${SCRIPT_SHELL_SUFFIX} ${build_command})
 
-    if(build_result EQUAL 0)
-        message(STATUS "build ${lib_name} success")
-    else()
-        message(FATAL_ERROR "build ${lib_name} failed")
+message(STATUS "build_command: ${build_command}")
+execute_process(
+    COMMAND build.${SCRIPT_SHELL_SUFFIX}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}
+    RESULT_VARIABLE build_result
+    OUTPUT_FILE ${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}/build.log
+)
+message(STATUS "work directory: ${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}")
+
+if(build_result EQUAL 0)
+    message(STATUS "build ${lib_name} success")
+else()
+    message(FATAL_ERROR "build ${lib_name} failed")
+endif()
+
+# install 
+set(install_command cmake --build ./build_${CMAKE_SYSTEM_NAME}/ --target install)
+
+if(__WIN__)
+    if(NOT DEFINED ${CMAKE_BUILD_TYPE})
+        set(CMAKE_BUILD_TYPE "Release")
     endif()
-
-    # install 
-    set(install_command cmake --build ./build/ --target install)
-
-    if(__WIN__)
-        set(install_command cmake --build ./build/ --target install --config ${CMAKE_BUILD_TYPE})
-    endif()
-
-    execute_process(
-        COMMAND ${install_command}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}
-        RESULT_VARIABLE install_result
-    )
     
-    message(STATUS "install command: ${install_command}")
-    if(install_result EQUAL 0)
-        message(STATUS "install ${lib_name} success")
-    else()
-        message(FATAL_ERROR "install ${lib_name} failed")
-    endif()
+    set(install_command cmake --build ./build_${CMAKE_SYSTEM_NAME}/ --target install --config ${CMAKE_BUILD_TYPE})
+endif()
+
+message(STATUS "install command: ${install_command}")
+execute_process(
+    COMMAND ${install_command}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${lib_name}
+    RESULT_VARIABLE install_result
+)
+
+if(install_result EQUAL 0)
+    message(STATUS "install ${lib_name} success")
+else()
+    message(FATAL_ERROR "install ${lib_name} failed")
+endif()
 
 endmacro(install_library)
 
